efa7b6cf6def94993a29af76afd686fb22f5675a
Kirill Maysner <kirillmajsner@gmail.com>
2019-09-13 11:05:40 +0600

    сверстал страницу модалку pp

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,74 +1,79 @@
 $(function () {
-  $('.toggler').click(function () {
-    $('.header').toggleClass('opened');
-  });
+    $('.toggler').click(function () {
+        $('.header').toggleClass('opened');
+    });
 
-  $('.dropdown').click(function () {
-    $(this).toggleClass('active');
-  });
+    $('.modal-close').click(function () {
+        $('.modal-box, .as').removeClass('opened-modal');
+    });
 
-  $('.points-body .point').on('click', function () {
-    $(this).toggleClass('opened');
-  });
 
-  $('.like-song').click(function () {
-    $(this).toggleClass('liked');
-  });
+    $('.dropdown').click(function () {
+        $(this).toggleClass('active');
+    });
 
-  initializeFirstScreenSlider();
-  initializePlaylistSliders();
+    $('.points-body .point').on('click', function () {
+        $(this).toggleClass('opened');
+    });
+
+    $('.like-song').click(function () {
+        $(this).toggleClass('liked');
+    });
+
+    initializeFirstScreenSlider();
+    initializePlaylistSliders();
 });
 
 const initializeFirstScreenSlider = () => {
-  const slider = $('.slider');
-  const currentSlideSpan = $('.counter #current');
-  const totalSlidesSpan = $('.counter #total');
+    const slider = $('.slider');
+    const currentSlideSpan = $('.counter #current');
+    const totalSlidesSpan = $('.counter #total');
 
-  slider.on('init reInit', function (event, slick, currentSlide, nextSlide) {
-    totalSlidesSpan.text(`0${slick.slideCount}`);
-  });
+    slider.on('init reInit', function (event, slick, currentSlide, nextSlide) {
+        totalSlidesSpan.text(`0${slick.slideCount}`);
+    });
 
 
-  slider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    let i = (currentSlide ? currentSlide : 0) + 1;
-    currentSlideSpan.text(`0${i}`);
-  });
+    slider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        let i = (currentSlide ? currentSlide : 0) + 1;
+        currentSlideSpan.text(`0${i}`);
+    });
 
-  slider.slick({
-    autoplay: true,
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 1200,
-    fade: true,
-    cssEase: 'linear',
-  });
+    slider.slick({
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 1200,
+        fade: true,
+        cssEase: 'linear',
+    });
 }
 
 const initializePlaylistSliders = () => {
-  $('.playlist-slider').slick({
-    mobileFirst: true,
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 320,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-        }
-      }
-    ]
-  });
+    $('.playlist-slider').slick({
+        mobileFirst: true,
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
+        ]
+    });
 }
